ff67a9567bf50ab428d285d215053b5b7c0d7104
forte <forte@is.mpg.de>
2024-10-10 19:14:40 +0200

    remove #

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 
-#echo -e "\nAfter registering at https://sgnify.is.tue.mpg.de/, provide your credentials:"
-#read -p "Username:" username
-#read -s -p "Password: " password
-#username=$(urle $username)
-#password=$(urle $password)
+echo -e "\nAfter registering at https://sgnify.is.tue.mpg.de/, provide your credentials:"
+read -p "Username:" username
+read -s -p "Password: " password
+username=$(urle $username)
+password=$(urle $password)
 
-#echo -e "\nDownloading SGNify..."
-#wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=sgnify&resume=1&sfile=data.zip' -O 'data.zip' --continue
-unzip data.zip
-rm data.zip
+echo -e "\nDownloading SGNify..."
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=sgnify&resume=1&sfile=data.zip' -O 'data.zip' --continue
 
 conda env create -f environment.yml 
 eval "$(conda shell.bash hook)"
 conda activate sgnify
+
+unzip data.zip
+rm data.zip
 
 git submodule update --init --recursive
 pip install --no-deps pyrender==0.1.23
